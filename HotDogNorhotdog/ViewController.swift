cda77c1e0fa2290ba7ade73c8d938233164b3448
Bayu Aslama Zulfikar Ahmad <bayuaslamaa@gmail.com>
2021-07-16 18:45:01 +0700

    finish image detection

--- HotDogNorhotdog/ViewController.swift
+++ HotDogNorhotdog/ViewController.swift
@@ -6,14 +6,76 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
     }
 
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert to CIImage")
+            }
+            detect(ciimage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(_ image: CIImage){
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading Core ML Model Faied.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+//            print(results)
+            let result = results.first
+            print(result)
+            if let confidence = result?.confidence as? Float {
+                if confidence >= 0.5 {
+                    self.title = result?.identifier
 
+                } else {
+                    let alert = UIAlertController(title: "Tidak Bisa Mendeteksi", message: "Silakan Ambil Kembali", preferredStyle: .alert)
+                    let okAction = UIAlertAction(title: "OK", style: .default) { action in
+                        self.present(self.imagePicker, animated: true, completion: nil)
+                    }
+                    alert.addAction(okAction)
+                    DispatchQueue.main.async {
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+    
+    @IBAction func cameraButtonDidTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
 
