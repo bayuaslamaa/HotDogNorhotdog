b5f831ed39f7ef38091ad9a272589cc8c758cbb1
Bayu Aslama Zulfikar Ahmad <bayuaslamaa@gmail.com>
2021-07-16 13:32:05 +0700

    add

---
+++ HotDogNorhotdog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotDogNorhotdog
+//
+//  Created by tollaut asset 02 on 16/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
